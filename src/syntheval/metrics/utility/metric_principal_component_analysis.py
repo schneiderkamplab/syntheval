2cd1cc3ac061180e74620527cfbdf075340e9880
Anton D. Lautrup <70313789+notna07@users.noreply.github.com>
2024-08-07 11:17:03 +0200

    update pca metric

--- src/syntheval/metrics/utility/metric_principal_component_analysis.py
+++ src/syntheval/metrics/utility/metric_principal_component_analysis.py
@@ -5,6 +5,8 @@
 import pandas as pd
 import numpy as np
 
+from typing import Literal
+
 from ..core.metric import MetricClass
 
 from sklearn.decomposition import PCA
@@ -37,21 +39,35 @@ class PrincipalComponentAnalysis(MetricClass):
         """ Set to 'privacy' or 'utility' """
         return 'utility'
 
-    def evaluate(self, num_components = 2) -> float | dict:
-        """ Function for evaluating the metric"""
+    def evaluate(self, num_components = 2, preprocess: Literal['mean', 'std'] = 'mean') -> float | dict:
+        """ Function for evaluating the metric
+        
+        Args:
+        num_components : int
+            Number of principal components to plot, the metrics are based on the first two regardless
+        preprocess : str
+            Preprocessing method for the data, either 'mean' or 'std'
+        """
+
         try:
-            assert (self.analysis_target is not None and self.analysis_target in self.cat_cols and len(self.num_cols)>=2)
+            assert ((not self.verbose or (self.analysis_target is not None and self.analysis_target in self.cat_cols)) and len(self.num_cols)>=2)
         except AssertionError:
-            if self.analysis_target is None: 
-                print('Error: Principal component analysis did not run, analysis class variable not set!')
-            elif len(self.num_cols)<2:
+            if len(self.num_cols)<2:
                 print('Error: Principal component analysis did not run, too few nummerical attributes!')
+            elif self.analysis_target is None: 
+                print('Error: Principal component analysis did not run, analysis class variable not set!')
             else:
                 print('Error: Principal component analysis did not run, provided class not in list of categoricals!')
             pass
         else:
-            r_scaled = StandardScaler().fit_transform(self.real_data[self.num_cols])
-            f_scaled = StandardScaler().fit_transform(self.synt_data[self.num_cols])
+            if preprocess == 'mean':
+                real_data = self.encoder.decode(self.real_data)
+                synt_data = self.encoder.decode(self.synt_data)
+                r_scaled = real_data[self.num_cols] - real_data[self.num_cols].mean()
+                f_scaled = synt_data[self.num_cols] - synt_data[self.num_cols].mean()
+            else:
+                r_scaled = StandardScaler().fit_transform(self.real_data[self.num_cols])
+                f_scaled = StandardScaler().fit_transform(self.synt_data[self.num_cols])
 
             pca = PCA(n_components=num_components)
             r_pca = pca.fit_transform(r_scaled)
@@ -69,7 +85,7 @@ class PrincipalComponentAnalysis(MetricClass):
 
             angle_diff = min([np.arccos(pca.components_[0] @ (s*synt_pca.components_[0])) for s in [1,-1]])/(len_r*len_f)
 
-            self.results = {'exp_var_diff': var_difference, 'comp_angle_diff': angle_diff}
+            self.results = {'exp_var_diff': var_difference/2, 'comp_angle_diff': 2*angle_diff/np.pi}
             if len(self.num_cols) > 5 :
                 r_sort = np.argsort(abs(pca.components_[0]))[::-1]
                 s_sort = np.argsort(abs(synt_pca.components_[0]))[::-1]
@@ -79,10 +95,12 @@ class PrincipalComponentAnalysis(MetricClass):
             r_pca = pd.DataFrame(r_pca,columns=labels)
             f_pca = pd.DataFrame(f_pca,columns=labels)
             s_pca = pd.DataFrame(s_pca,columns=labels)
-            r_pca['target'] = self.real_data[self.analysis_target]
-            f_pca['target'] = self.synt_data[self.analysis_target]
-            if self.verbose: plot_principal_components(r_pca,f_pca)
-            if self.verbose: plot_own_principal_component_pairplot(stack(r_pca,s_pca))
+
+            if self.verbose: 
+                r_pca['target'] = self.real_data[self.analysis_target]
+                f_pca['target'] = self.synt_data[self.analysis_target]
+                plot_principal_components(r_pca,f_pca)
+                plot_own_principal_component_pairplot(stack(r_pca,s_pca))
             return self.results
 
     def format_output(self) -> str:
@@ -92,7 +110,7 @@ class PrincipalComponentAnalysis(MetricClass):
         if self.results != {}:
             string = """\
 | PCA difference in eigenvalues (exp. var.):   %.4f           |
-| PCA angle between eigenvectors (radians) :   %.4f           |""" % (self.results['exp_var_diff'], 
+| PCA angle diff. between eigenvectors     :   %.4f           |""" % (self.results['exp_var_diff'], 
                                                                       self.results['comp_angle_diff'])
             return string
         else: pass
@@ -113,7 +131,7 @@ class PrincipalComponentAnalysis(MetricClass):
                      },
                      {'metric': 'pca_eigvec_ang', 'dim': 'u', 
                      'val': self.results['comp_angle_diff'], 
-                     'n_val': 1-self.results['comp_angle_diff']/np.pi 
+                     'n_val': 1-self.results['comp_angle_diff'] 
                      }]
         else: pass
 
