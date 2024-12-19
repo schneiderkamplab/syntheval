2b7e50c836ae0921517c4c84fdafb0885b55bae1
Anton D. Lautrup <70313789+notna07@users.noreply.github.com>
2024-12-19 14:50:30 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='syntheval',
-    version='1.5.1',
+    version='1.5.2',
     packages=['syntheval'],#find_packages(),
     include_package_data=True,
     install_requires=[
